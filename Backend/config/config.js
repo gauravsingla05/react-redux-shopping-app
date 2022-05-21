f5c8223e439ba1f0c0ca8e5a22e360031668b7eb
gourav singla <autobytesolutions@gmail.com>
2022-05-21 16:03:51 -0500

    5/21/2022

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,24 +2,18 @@ module.exports = {
     secret: 'qpalskdjfhgtyrueibvnxjj4676'
   };
 
-  module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD:'Mock@tests@061',
-  DB: 'shoppy'
-};
-
-// module.exports = {
-//   HOST: 'localhost',
-//   USER: 'autobyte_root',
-//   PASSWORD:'Mock@tests@061',
-//   DB: 'autobyte_shoppy'
-// };
-
-// module.exports = {
+//   module.exports = {
 //   HOST: 'localhost',
 //   USER: 'root',
 //   PASSWORD:'Mock@tests@061',
 //   DB: 'shoppy'
 // };
+
+
+module.exports = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD:'password',
+  DB: 'shoppy'
+};
 
--- client/src/apis/config.js
+++ client/src/apis/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/'
+// export const BASE_URL = 'http://localhost:5000/'
+export const BASE_URL = 'http://13.235.68.235:5000/'
